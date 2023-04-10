805ad7958e89f188e361e36c6b9dc1f21ab3c678
Hakon Sveinsson <adlera2001is@gmail.com>
2023-04-10 05:53:03 +0000

    Committing

--- BoredlessTourist.py
+++ BoredlessTourist.py
@@ -7,6 +7,11 @@ def get_destination_index(destination):
     destination_index = destinations.index(destination)
     return destination_index
 
-print(get_destination_index("Los Angeles, USA"))
+# print(get_destination_index("Los Angeles, USA"))
+
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
 
 
